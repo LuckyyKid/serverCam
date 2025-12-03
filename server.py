b3f9e74a70743459e152875b307ec7db9822187a
Bafing keita <2187902@crosemont.qc.ca>
2025-12-03 09:03:34 -0500

    Initial commit - server camera API

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+last_alert = None
+
+@app.route("/alert", methods=["POST"])
+def alert():
+    global last_alert
+    last_alert = request.json
+    print("Nouvelle alerte reçue :", last_alert)
+    return {"status": "received"}
+
+@app.route("/alert/next", methods=["GET"])
+def get_alert():
+    global last_alert
+    if last_alert:
+        data = last_alert
+        last_alert = None
+        return {"alert": data}
+    return {"alert": None}
